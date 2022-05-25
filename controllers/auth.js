c27417273e239476f8dca801a1ba6becc045e50c
mausam107 <mausam2001kv@gmail.com>
2022-05-25 21:22:13 +0530

    edited

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,8 @@ const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 
+const { validationResult }=require('express-validator/check');
+
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
   if (message[0] == "Invalid email or password.") {
@@ -67,6 +69,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors=validationResult(req);
+  if(!errors.isEmpty()){
+    console.log(errors.array());
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,52 @@
-const express = require('express');
+const express = require("express");
+const { check, body } = require("express-validator/check");
 
-const authController = require('../controllers/auth');
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
+router.get("/login", authController.getLogin);
 
-router.get('/signup', authController.getSignup);
+router.get("/signup", authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post("/login", authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        if (value === "123@gmail.com") {
+          throw new Error("This email address if forbidden.");
+        }
+        return true;
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and min 5 characters."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPasssword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match!");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
 
-router.post('/logout', authController.postLogout);
+router.post("/logout", authController.postLogout);
 
-router.get('/reset', authController.getReset);
+router.get("/reset", authController.getReset);
 
-router.post('/reset', authController.postReset);
+router.post("/reset", authController.postReset);
 
-router.get('/reset/:token', authController.getNewPassword);
+router.get("/reset/:token", authController.getNewPassword);
 
-router.post('/reset/:token', authController.postNewPassword);
-
+router.post("/reset/:token", authController.postNewPassword);
 
 module.exports = router;
