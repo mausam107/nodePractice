8f5ddd163da191e625ff0a6ad2a0e25a9d04edfa
mausam107 <mausam2001kv@gmail.com>
2022-05-27 23:37:01 +0530

    edited

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const express = require('express');
 
+const { body } = require("express-validator/check");
+
 const adminController = require('../controllers/admin');
 
 const isAut=require('../middleware/is-aut');
@@ -11,14 +13,41 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product',isAut,adminController.getAddProduct);
 
-router.post('/add-product',isAut, adminController.postAddProduct);
+router.post('/add-product',[
+    body('title')
+        .isString()
+        .isLength({min:3})
+        .trim()
+        ,
+    body('imageUrl')
+        .isURL(),
+    body('price')
+        .isFloat(),
+    body('description')
+    .isLength({min:5,max:400})
+    .trim()
+],
+isAut, adminController.postAddProduct);
 
 // // /admin/products => GET
 router.get('/products',isAut, adminController.getProducts);
 
 router.get('/edit-product/:productId',isAut, adminController.getEditProduct);
 
-router.post('/edit-product',isAut, adminController.postEditProduct);
+router.post('/edit-product',[
+    body('title')
+        .isString()
+        .isLength({min:3})
+        .trim(),
+    body('imageUrl')
+        .isURL(),
+    body('price')
+        .isFloat(),
+    body('description')
+        .isLength({min:5,max:400})
+        .trim()
+],
+isAut, adminController.postEditProduct);
 
 router.post('/delete-product',isAut,adminController.postDeleteProduct);
 module.exports = router;
